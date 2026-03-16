ff02a92a51b40875321279afb201332221d84fc2
Isra de Leon <isra.frontend@gmail.com>
2026-03-16 11:04:57 -0600

    refactor to fetch questions use case impl

---
+++ app/src/main/java/com/techyourchance/architecture/domain/use_cases/FetchQuestionsUseCase.kt
@@ -0,0 +1,59 @@
+package com.techyourchance.architecture.domain.use_cases
+
+import com.techyourchance.architecture.BuildConfig
+import com.techyourchance.architecture.common.network.StackoverflowApi
+import com.techyourchance.architecture.domain.question.QuestionSchema
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class FetchQuestionsUseCase {
+    private val retrofit by lazy {
+        val httpClient = OkHttpClient.Builder().run {
+            addInterceptor(HttpLoggingInterceptor().apply {
+                if (BuildConfig.DEBUG) {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            })
+            build()
+        }
+
+        Retrofit.Builder()
+            .baseUrl("http://api.stackexchange.com/2.3/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(httpClient)
+            .build()
+    }
+
+    private val stackoverflowApi by lazy {
+        retrofit.create(StackoverflowApi::class.java)
+    }
+
+    private var questions: List<QuestionSchema> = emptyList()
+
+    suspend fun fetch(
+        forceUpdate: Boolean = false
+    ): List<QuestionSchema> {
+        return if (hasEnoughTimePassed() || forceUpdate)
+            withContext(Dispatchers.IO) {
+                questions = stackoverflowApi.fetchLastActiveQuestions(20)!!.questions
+                lastNetworkRequestInNanoS = System.nanoTime()
+                questions
+            }
+        else questions
+    }
+
+    private var lastNetworkRequestInNanoS = 0L
+
+    private fun hasEnoughTimePassed(): Boolean {
+        return System.nanoTime() - lastNetworkRequestInNanoS > THROTTLE_TIME_OUT
+    }
+
+    companion object {
+        // Milliseconds to nano seconds conversion
+        const val THROTTLE_TIME_OUT = 5000L * 1_000_000L
+    }
+}
--- app/src/main/java/com/techyourchance/architecture/ui/questions_list/QuestionsListViewModel.kt
+++ app/src/main/java/com/techyourchance/architecture/ui/questions_list/QuestionsListViewModel.kt
@@ -1,58 +1,20 @@
 package com.techyourchance.architecture.ui.questions_list
 
 import androidx.lifecycle.ViewModel
-import com.techyourchance.architecture.BuildConfig
-import com.techyourchance.architecture.common.network.StackoverflowApi
 import com.techyourchance.architecture.domain.question.QuestionSchema
+import com.techyourchance.architecture.domain.use_cases.FetchQuestionsUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 class QuestionsListViewModel: ViewModel() {
-    private val retrofit by lazy {
-        val httpClient = OkHttpClient.Builder().run {
-            addInterceptor(HttpLoggingInterceptor().apply {
-                if (BuildConfig.DEBUG) {
-                    level = HttpLoggingInterceptor.Level.BODY
-                }
-            })
-            build()
-        }
-
-        Retrofit.Builder()
-            .baseUrl("http://api.stackexchange.com/2.3/")
-            .addConverterFactory(MoshiConverterFactory.create())
-            .client(httpClient)
-            .build()
-    }
-
-    private val stackoverflowApi by lazy {
-        retrofit.create(StackoverflowApi::class.java)
-    }
+    val fetchQuestionsUseCase = FetchQuestionsUseCase()
 
     val questions = MutableStateFlow<List<QuestionSchema>>(emptyList())
-    private var lastNetworkRequestInNanoS = 0L
 
-    private fun hasEnoughTimePassed(): Boolean {
-        return System.nanoTime() - lastNetworkRequestInNanoS > THROTTLE_TIME_OUT
-    }
-
-    suspend fun fetchQuestions(
-        forceUpdate: Boolean = false
-    ) {
-        if (hasEnoughTimePassed() || forceUpdate)
-            withContext(Dispatchers.Main.immediate) {
-                questions.value = stackoverflowApi.fetchLastActiveQuestions(20)!!.questions
-                lastNetworkRequestInNanoS = System.nanoTime()
-            }
-    }
-
-    companion object {
-        // Milliseconds to nano seconds conversion
-        const val THROTTLE_TIME_OUT = 5000L * 1_000_000L
+    suspend fun fetchQuestions() {
+        withContext(Dispatchers.Main.immediate) {
+            questions.value = fetchQuestionsUseCase.fetch()
+        }
     }
 }
